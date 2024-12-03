ff22ea16149d3741946102f0d56b78ea1b3f75e3
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-12-03 10:20:05 -0800

    hdgdge

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,32 +1,133 @@
 #include "binary_trees.h"
 
 /**
- * binary_trees_ancestor - function that finds the lowest common ancest.
- * @first: pointer to the first node.
- * @second: pointer to the second node.
- * Return: pointer to the lowest common ancestor node,.
+ * binary_tree_height - Function that measures the height of a binary tree
+ * @tree: tree to go through
+ * Return: the height
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	binary_tree_t *first_ancestor = (binary_tree_t *)first;
-	binary_tree_t *second_ancestor = (binary_tree_t *)second;
+    size_t l = 0;
+    size_t r = 0;
 
-	/* Traverse up from first node and store its ancestors */
-	while (first_ancestor)
-	{
-		second_ancestor = (binary_tree_t *)second;
-		while (second_ancestor)
-		{
-			/* Check if the ancestors are the same */
-			if (first_ancestor == second_ancestor)
-			{
-				return (first_ancestor);
-			}
-			second_ancestor = second_ancestor->parent;
-		}
-		first_ancestor = first_ancestor->parent;
-	}
+    if (tree == NULL)
+    {
+        return (0);
+    }
+    else
+    {
+        if (tree)
+        {
+            l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+            r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+        }
+        return ((l > r) ? l : r);
+    }
+}
 
-	/* If no common ancestor is found, return NULL */
-	return (NULL);
+/**
+ * binary_tree_depth - depth of specified node from root
+ * @tree: node to check the depth
+ * Return: 0 if it is the root or number of depth
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+    return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+}
+
+/**
+ * linked_node - this function makes a linked list from depth level and node
+ * @head: pointer to head of linked list
+ * @tree: node to store
+ * @level: depth of node to store
+ * Return: Nothing
+ */
+void linked_node(link_t **head, const binary_tree_t *tree, size_t level)
+{
+    link_t *new, *aux;
+
+    new = malloc(sizeof(link_t));
+    if (new == NULL)
+    {
+        return;
+    }
+    new->n = level;
+    new->node = tree;
+    if (*head == NULL)
+    {
+        new->next = NULL;
+        *head = new;
+    }
+    else
+    {
+        aux = *head;
+        while (aux->next != NULL)
+        {
+            aux = aux->next;
+        }
+        new->next = NULL;
+        aux->next = new;
+    }
+}
+
+/**
+ * recursion - goes through the complete tree and stores each node
+ * in linked_node function
+ * @head: pointer to head of linked list
+ * @tree: node to check
+ * Return: Nothing by default it affects the pointer
+ */
+void recursion(link_t **head, const binary_tree_t *tree)
+{
+    size_t level = 0;
+
+    if (tree != NULL)
+    {
+        level = binary_tree_depth(tree);
+        linked_node(head, tree, level);
+        recursion(head, tree->left);
+        recursion(head, tree->right);
+    }
+}
+
+/**
+ * binary_tree_levelorder - print the nodes element in level-order
+ * @tree: root node
+ * @func: function to use
+ * Return: Nothing
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+    link_t *head, *aux;
+    size_t height = 0, count = 0;
+
+    if (!tree || !func)
+    {
+        return;
+    }
+    else
+    {
+        height = binary_tree_height(tree);
+        head = NULL;
+        recursion(&head, tree);
+        while (count <= height)
+        {
+            aux = head;
+            while (aux != NULL)
+            {
+                if (count == aux->n)
+                {
+                    func(aux->node->n);
+                }
+                aux = aux->next;
+            }
+            count++;
+        }
+        while (head != NULL)
+        {
+            aux = head;
+            head = head->next;
+            free(aux);
+        }
+    }
 }
