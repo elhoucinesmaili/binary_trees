879c49e7171ebd45517c03b6aff7a2118937cbf3
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-12-05 08:15:15 -0800

    gdgd

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -1,50 +1,60 @@
 #include "binary_trees.h"
 
 /**
- * bst_insert - Function that inserts a value in a Binary Search Tree
- * @root: Double pointer to the root node of the BST to insert the value
- * @value: The value to store in the node to be inserted
+ * insert_to_max_heap - Inserts a value into a Max Binary Heap.
+ * @root: Double pointer to the root node of the Heap.
+ * @value: The value to store in the node to be inserted.
  *
- * Return: Pointer to the new node, or NULL if the value is already in the tree
+ * Return: Pointer to the newly created node, or NULL on failure.
  */
-bst_t *bst_insert(bst_t **root, int value)
+heap_t *insert_to_max_heap(heap_t **root, int value)
 {
-	bst_t *tree, *new_node;
+    heap_t *current, *new_node, *swap_node;
+    int total_nodes, remaining_nodes, temp, level, mask;
 
-	if (!root)
-		return (NULL);
+    if (!root)
+        return (NULL);
+    if (!(*root))
+        return (*root = binary_tree_node(NULL, value));
 
-	if (!(*root))
-		return (*root = binary_tree_node(NULL, value));
+    current = *root;
+    total_nodes = calculate_tree_size(current);
+    remaining_nodes = total_nodes;
 
-	tree = *root;
+    for (level = 0, mask = 1; remaining_nodes >= mask; mask <<= 1, level++)
+        remaining_nodes -= mask;
 
-	/* Traverse the tree to find the correct position for the new node */
-	while (tree)
-	{
-		if (value < tree->n)
-		{
-			if (!tree->left)
-			{
-				new_node = binary_tree_node(tree, value);
-				tree->left = new_node;
-				return (new_node);
-			}
-			tree = tree->left;
-		}
-		else if (value > tree->n)
-		{
-			if (!tree->right)
-			{
-				new_node = binary_tree_node(tree, value);
-				tree->right = new_node;
-				return (new_node);
-			}
-			tree = tree->right;
-		}
-		else
-			return (NULL);  /* Value already exists, no duplicates allowed */
-	}
+    for (mask = 1 << (level - 1); mask > 1; mask >>= 1)
+        current = (remaining_nodes & mask) ? current->right : current->left;
 
-	return (NULL);
+    new_node = binary_tree_node(current, value);
+    if (remaining_nodes & 1)
+        current->right = new_node;
+    else
+        current->left = new_node;
+
+    swap_node = new_node;
+    while (swap_node->parent && (swap_node->n > swap_node->parent->n))
+    {
+        temp = swap_node->n;
+        swap_node->n = swap_node->parent->n;
+        swap_node->parent->n = temp;
+        new_node = new_node->parent;
+    }
+
+    return (new_node);
+}
+
+/**
+ * calculate_tree_size - Calculates the size of a binary tree.
+ * @tree: Pointer to the root node of the tree.
+ *
+ * Return: Size of the tree, or 0 if the tree is NULL.
+ */
+size_t calculate_tree_size(const binary_tree_t *tree)
+{
+    if (!tree)
+        return (0);
+
+    return (calculate_tree_size(tree->left) + calculate_tree_size(tree->right) + 1);
 }
